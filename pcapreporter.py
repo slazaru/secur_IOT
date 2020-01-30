51c5198d29f2d9224d9ddbfbe45a24c31d2b6bb0
root <root@c0cc29e81f4f>
2020-01-30 04:18:05 +0000

    pcapreporter

--- pcapreporter.py
+++ pcapreporter.py
@@ -1,17 +1,11 @@
+#!/usr/bin/env python3
+
 # coordinator for makeclouds and pcapgrok
 
 # location of pcapgrok main.py
-pcapgrokmain = "/home/pi/opt/pcapGrok/pcapGrok.py"
-# location of pcapgrok hosts file
-hostsfile = "/home/pi/secur_IOT/pcapgrok_hosts.xls"
+pcapgrokmain = "/root/pcapGrok/pcapGrok.py"
 # location of makeclouds.py
-makeclouds = "/home/pi/secur_IOT/makeclouds.py"
-
-# add name:mac addresses pairs of devices you want to graph here
-# mac addresses can be retrieved from zeek dhcp logs, or by using nmap
-addresses = [("device_under_test","00:0c:43:9d:3d:25"),
-("Phone","00:ec:0a:ca:e9:ea"),
-("Laptop", "a0:d3:7a:d9:d1:90")]
+makeclouds = "/root/secur_IOT/makeclouds.py"
 
 import argparse
 import os
@@ -20,11 +14,17 @@ from pathlib import Path
 from shutil import rmtree
 
 parser = argparse.ArgumentParser(description='Pcap report generator')
-parser.add_argument('pcap', help='the pcap to process')
-parser.add_argument('dir', help='the directory to save the output')
+parser.add_argument('pcap', help='Tthe pcap to process')
+parser.add_argument('-d', '--dir', help='The directory to save the output. By default it is saved to /var/www/html/PCAPFILENAME')
+parser.add_argument('-hf', '--hostsfile', help='The hostsfile to use. The hostsfile labels the nodes in the graphs produced. It is recommended to include a hostsfile. An example hostsfile can be found in /root/pcapGrok/example_hostsfile.xls')
 args = parser.parse_args()
-dir = os.path.abspath(args.dir)
+
 infname = os.path.abspath(args.pcap)
+if args.dir:
+    dir = os.path.abspath(args.dir)
+else:
+    dir = os.path.join('/var/www/html/', os.path.basename(infname))
+print("dir is " + dir)
 
 def wordclouds():
     cmd = []
@@ -33,11 +33,7 @@ def wordclouds():
     cmd.append(infname)
     cmd.append(dir)
     print("Running " + " ".join(cmd))
-    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr = subprocess.PIPE)
-    p.wait()
-    if p.stderr:
-	    for line in p.stderr:
-	        print(line.strip())
+    subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr = subprocess.PIPE, shell=False)
 
 def pcapgrok(hf=None, maxnodes=None, restrictmac=None):
     if restrictmac == None:
@@ -45,8 +41,6 @@ def pcapgrok(hf=None, maxnodes=None, restrictmac=None):
     else:
         suffix = restrictmac[0] + "_" +restrictmac[1]
     newdir = os.path.join(dir, suffix)
-    if os.path.exists(newdir):
-        rmtree(newdir)
     p = Path(newdir)
     p.mkdir(mode=0o755, parents=True, exist_ok=True)
     cmd = []
@@ -74,14 +68,9 @@ def pcapgrok(hf=None, maxnodes=None, restrictmac=None):
         cmd.append("_" + restrictmac[0] + ".pdf")
     else:
         cmd.append(".pdf")
-    print("Running " + " ".join(cmd))
-    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr = subprocess.PIPE)
+    print("\nRunning " + " ".join(cmd))
+    p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr = subprocess.PIPE, shell=False)
     p.wait()
-    for line in p.stdout:
-        print(line.decode('ascii').strip())
-    if p.stderr:
-        for line in p.stderr:
-            print(line.strip())
     if restrictmac is not None:
         reportfname = os.path.join(dir, restrictmac[0] + ".html")
     else:
@@ -93,17 +82,34 @@ def pcapgrok(hf=None, maxnodes=None, restrictmac=None):
         f.write("<embed src=\"" + suffix + "/" + file + "\" type=\"application/pdf\" width=\"98%\" height=\"100%\">\n")
     f.write("<\/html>")
     f.close()
+    print("\nreportfname : " + reportfname)
+    print("\nnewdir : " + newdir)
+    print("\nreport written to " + reportfname)
 
-# delete all existing files
-if os.path.exists(dir):
-    rmtree(dir)
 # make path if it doesn't exist
 p = Path(dir)
 p.mkdir(mode=0o755, parents=True, exist_ok=True)
 
-#main
-
 #wordclouds()
-pcapgrok(hostsfile,2)
-for pair in addresses:
-    pcapgrok(hostsfile,2,pair)
+if args.hostsfile:
+    print("\nhostsfile was included")
+    # run with MAC address restrictions per line in hostsfile
+    f = open(args.hostsfile, 'r')
+    for line in f:
+        line = line.split(',')
+        if line[0] == 'ip':
+            continue # skip header line
+        if len(line) != 6:
+            print("\nimproperly formatted line in supplied hostsfile!")
+            continue
+        ipaddr = line[0].strip()
+        macaddr = line[5].strip()
+        name = line[1].strip()
+        pair = (name, macaddr)
+        pcapgrok(args.hostsfile, 2, pair)
+    # run once without MAC address restrictions
+    pcapgrok(args.hostsfile,2)
+else:
+    print("\nWARNING: no hostsfile supplied. it is recommended to run this program using a hostsfile. for more information, run with -h")
+    pcapgrok(None,2)
+print("\ndone!")
